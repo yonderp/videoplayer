838bcd18f017171eb6bdb10beb20fb4d38980e73
Yonder Peña <djyonder@gmail.com>
2021-09-24 20:19:59 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,66 @@
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// pLay & pause video
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// update pLay/pause icon
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
+}
+
+// Update progress & timestamp
+function updateProgress() {
+    progress.value = (video.currentTime / video.duration) * 100; // console.log(video.duration);
+
+    //Get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if (mins < 10) {
+        mins = '0' + String(mins);
+    }
+
+    //Get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if (secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+    video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+// Stop video
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+// Even listeners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+
+stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setVideoProgress);
+
